4f90f40c29a7a018b38d245e16869479ef7bf83e
thanhbao215 <bao4551050004@st.qnu.edu.vn>
2023-11-20 21:11:32 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        abc zxj = new abc();
+        zxj.setName("Thành Bảo");
+        System.out.println(zxj.getName());
+    }
+}
---
+++ src/abc.java
@@ -0,0 +1,17 @@
+public class abc {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abc(String name) {
+        this.name = name;
+    }
+
+    public abc(){}
+}
